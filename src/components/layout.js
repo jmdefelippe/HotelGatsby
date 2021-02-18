e6eb552d45fd34f671148e4d920247c43d88c231
Joan <jmdefelippe@gmail.com>
2021-02-18 17:39:43 -0300

    Header

---
+++ src/components/layout.js
@@ -0,0 +1,48 @@
+import React from 'react';
+import Helmet from 'react-helmet';
+import { Global, css } from '@emotion/react';
+import Header from './header';
+
+const Layout = (props) => {
+    return (
+        <>
+            <Global
+                styles={css`
+                    html {
+                        font-size: 62.5%;
+                    }
+                    body {
+                        font-size: 16px;
+                        font-size: 1.6rem;
+                        line-height: 1.5;
+                    }
+                    h1, h2, h3 {
+                        margin: 0;
+                        line-height: 1.5;
+                    }
+                    h1, h2 {
+                        font-family: 'Roboto', serif;
+                    }
+                    h3 {
+                        font-family: 'PT Sans', sans-serif;
+                    }
+                    ul {
+                        list-style: none;
+                        margin: 0;
+                        padding: 0;
+                    }
+                `}
+            />
+            <Helmet>
+                <title>Gatsby Hotel</title>
+                <link href="https://cdnjs.cloudflare.com/ajax/libs/normalize/8.0.1/normalize.css" rel="stylesheet" />
+                <link href="https://fonts.googleapis.com/css2?family=PT+Sans:wght@400;700&family=Roboto:wght@400;700&display=swap" rel="stylesheet" />
+            </Helmet>
+            
+            <Header />
+            {props.children}
+        </>
+    );
+}
+
+export default Layout;
